36cd39c56f097c3e8331e7ba34d8b401e00d4c69
Mohannish <saimmohanish@gmail.com>
2026-03-12 16:02:32 +0530

    Notes Application Form handling Two way binding

--- 09-Note-App project/src/App.jsx
+++ 09-Note-App project/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (e) => {
+    e.preventDefault();
+    console.log("Adding Note", { title, description });
+    setTitle("");
+    setDescription("");
+    setNotes((prevNotes) => [
+      ...prevNotes,
+      title && description && { title, description },
+    ]);
+    console.log("Updated Notes:", notes);
+  };
+
+  const deleteNote = (idx) => {
+    notes.splice(idx, 1);
+    setNotes([...notes]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="bg-yellow-200 h-screen lg:flex">
+      <form
+        onSubmit={(e) => addNote(e)}
+        className="flex flex-col items-start gap-4 p-10 lg:w-1/2"
+        action=""
+      >
+        <h1 className="text-3xl font-bold">Add Notes</h1>
+        <input
+          className="p-2 border-2 rounded w-full"
+          type="text"
+          placeholder="Enter note heading"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          className="p-2 border-2 rounded h-32 w-full"
+          placeholder="Enter note description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <button
+          type="submit"
+          className="bg-black py-2 px-4 rounded text-white w-full active:scale-95 transition-colors"
+        >
+          Add Note
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      </form>
+      <div className="p-10 lg:border-l-2 bg-gray-400 lg:w-1/2">
+        <h1 className="text-3xl font-bold">Your Notes</h1>
+        <div className="flex flex-wrap items-start justify-start gap-3 mt-5 overflow-auto h-[95%] scrollbar-hide">
+          {notes.map((note, idx) => {
+            return (
+              <div
+                key={idx}
+                className=" h-52 flex flex-col justify-between px-4 py-7 bg-cover w-40 rounded-2xl bg-center bg-[url('https://marketplace.canva.com/EAGq4e4Rb4E/1/0/1131w/canva-purple-and-white-clean-lines-notes-flyer-7gTxcKZdBQM.jpg')]"
+              >
+                <div className="overflow-auto h-[95%]">
+                  <h2 className="text-lg font-medium pt-4 leading-tight">
+                    {note.title}
+                  </h2>
+                  <p className="px-2 py-1">{note.description}</p>
+                </div>
+                <button
+                  onClick={() => deleteNote(idx)}
+                  className="bg-red-500 text-white rounded cursor-pointer active:scale-95"
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
